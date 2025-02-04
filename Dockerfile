3787622c432e9f0fe922b8dc3af5722e5b660d91
Shaw <avishek.b.shaw@accenture.com>
2025-02-04 23:10:04 +0530

    feat:revision8

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 FROM nginx:latest
 
-RUN echo "Hello World!" > /usr/share/nginx/html/index.html 
+# Copy HTML and JavaScript files into the container
+COPY app/index.html /usr/share/nginx/html/index.html
+COPY app/app.js /usr/share/nginx/html/app.js
+
+# Expose port 80 for the web server
+EXPOSE 80
---
+++ app/app.js
@@ -0,0 +1,26 @@
+// Function to get a greeting message based on the time of day
+function getGreeting() {
+    let hours = new Date().getHours();
+    if (hours < 12) return "Good Morning!";
+    else if (hours < 18) return "Good Afternoon!";
+    else return "Good Evening!";
+}
+
+// Function to get the current location using the Geolocation API
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            document.getElementById("location").innerText = 
+                "Your location: Latitude " + position.coords.latitude + ", Longitude " + position.coords.longitude;
+        });
+    } else {
+        document.getElementById("location").innerText = "Location access denied.";
+    }
+}
+
+// Set the dynamic content when the page loads
+window.onload = function() {
+    document.getElementById("greeting").innerText = getGreeting();
+    getLocation();
+    document.getElementById("datetime").innerText = new Date().toLocaleString();
+};
